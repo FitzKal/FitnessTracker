4a624fc454c2ac867caff03afe88bd7fa8285370
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-10-19 20:28:49 +0200

    Implemented service methods in the AuthenticationController

--- src/main/java/com/undieb/hu/main/Controllers/AuthenticationController.java
+++ src/main/java/com/undieb/hu/main/Controllers/AuthenticationController.java
@@ -1,9 +1,14 @@
 package com.undieb.hu.main.Controllers;
 
+import com.undieb.hu.main.Controllers.DTOs.LoginRequestDTO;
+import com.undieb.hu.main.Controllers.DTOs.LoginUserResponseDTO;
+import com.undieb.hu.main.Controllers.DTOs.RegisterUserDto;
 import com.undieb.hu.main.Services.AuthenticationService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.NonNull;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -11,5 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
-    
+    @PostMapping("/register")
+    public ResponseEntity<LoginUserResponseDTO> registerUser(@NonNull @RequestBody RegisterUserDto registerUserDto){
+        return ResponseEntity.ok(authenticationService.registerUser(registerUserDto));
+    }
+
+    @GetMapping("/login")
+    public ResponseEntity<LoginUserResponseDTO> loginUser(@NonNull @RequestBody LoginRequestDTO requestDTO){
+        return ResponseEntity.ok(authenticationService.loginUser(requestDTO));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logoutUser(HttpServletRequest request){
+        authenticationService.logout(request);
+        return ResponseEntity.ok("You have been successfully logged out!");
+    }
+
 }
