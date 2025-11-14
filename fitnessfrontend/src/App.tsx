262b9af80a57420ba5f4829f23a693b104eaf2d2
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-11-14 15:29:23 +0100

    Added react toastify

--- fitnessfrontend/src/App.tsx
+++ fitnessfrontend/src/App.tsx
@@ -1,7 +1,12 @@
+import {ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
 function App() {
 
   return (
     <div>
+        <ToastContainer />
     </div>
   )
 }
