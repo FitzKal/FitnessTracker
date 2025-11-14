e725fa523aed0cf2e47ee28b83bd1065a43c72e5
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-11-14 15:35:19 +0100

    Added queryClient and BrowserRouter

--- fitnessfrontend/src/main.tsx
+++ fitnessfrontend/src/main.tsx
@@ -1,9 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+const queryClient = new QueryClient();
+const router = createBrowserRouter([
+
+]);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <App />
+      </QueryClientProvider>
   </StrictMode>,
 )
