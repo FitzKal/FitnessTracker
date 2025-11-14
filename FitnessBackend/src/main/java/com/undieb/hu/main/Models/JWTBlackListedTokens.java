53acdbffc91ccec5e128779f8527e396d6bfa4f1
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-11-14 13:33:17 +0100

    Created thr JWTBlackListedTokens entity for saving blacklisted tokens more efficiently and not in-memory.

---
+++ FitnessBackend/src/main/java/com/undieb/hu/main/Models/JWTBlackListedTokens.java
@@ -0,0 +1,22 @@
+package com.undieb.hu.main.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class JWTBlackListedTokens {
+    @Id
+    private String token;
+    private Date expirationDate;
+
+}
