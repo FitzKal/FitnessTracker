a8e08bd701bb971c4bc421f0b83311fe37f82e86
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-11-18 16:34:09 +0100

    converted the fetch requests to axios requests

--- fitnessfrontend/src/services/AuthService.ts
+++ fitnessfrontend/src/services/AuthService.ts
@@ -1,41 +1,32 @@
-// ------------- LOGIN -------------
+import api from "./AxiosConfig.tsx";
 import type {LoginRequest, RegisterRequest} from "../types/FormTypes.ts";
 
+// ------------- LOGIN -------------
 export const userLogin = async (loginRequest:LoginRequest) =>{
-    const res = await fetch("/api/fitness/auth/login",{
-        method:"POST",
-        headers:{
-            "Content-Type" : "application/json"
-        },
-        body:JSON.stringify(loginRequest)
-    });
-    if (res.ok){
-        const response = await res.json();
-        console.log(response);
-        return response;
+    try {
+        const res = await api.post("/auth/login",loginRequest)
+            .then(response =>response.data);
+        console.log(res);
+        return res;
+    }catch (error){
+        const message = (error as Error).message;
+        console.log(message)
+        throw new Error("Incorrect login credentials");
     }
-    else{
-        const message = await res.text();
-        throw new Error(message || "Something went wrong");
-    }
+
 }
+
 
 // ------------- Register -------------
 export const registerUser = async (registerRequest:RegisterRequest) =>{
-    const res = await fetch("/api/fitness/auth/register",{
-        method:"POST",
-        headers:{
-            "Content-Type" : "application/json"
-        },
-        body:JSON.stringify(registerRequest)
-    });
-    if (res.ok){
-        const response = await res.json();
-        console.log(response);
-        return response;
-    }
-    else{
-        const message = await res.text();
-        throw new Error(message || "Something went wrong");
+    try{
+        const res = await api.post("/auth/register",registerRequest)
+            .then(response => response.data);
+        console.log(res);
+        return res
+    }catch (error){
+        const message = (error as Error).message;
+        console.log(message);
+        throw new Error("There was a problem while registering");
     }
 }
