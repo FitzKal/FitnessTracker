00a0b53359305dd62214fbdfc6fc6ef464344642
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-11-18 21:40:50 +0100

    Updated the register method in the controller

--- FitnessBackend/src/main/java/com/undieb/hu/main/Services/AuthenticationService.java
+++ FitnessBackend/src/main/java/com/undieb/hu/main/Services/AuthenticationService.java
@@ -3,33 +3,55 @@ import com.undieb.hu.main.Controllers.DTOs.LoginRequestDTO;
 import com.undieb.hu.main.Controllers.DTOs.LoginUserResponseDTO;
 import com.undieb.hu.main.Controllers.DTOs.RegisterUserDto;
 import com.undieb.hu.main.Converters.RegisterUserDTOToUserConverter;
+import com.undieb.hu.main.Exceptions.InvalidVerificationCodeException;
 import com.undieb.hu.main.Exceptions.UserNotFoundException;
 import com.undieb.hu.main.Models.Role;
 import com.undieb.hu.main.Models.Users;
 import com.undieb.hu.main.Repositories.UserRepository;
 import com.undieb.hu.main.Security.PasswordEncrypter;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.AllArgsConstructor;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
+@Data
 public class AuthenticationService {
+    @Autowired
     private final UserRepository userRepository;
+    @Autowired
     private final PasswordEncrypter passwordEncrypter;
+    @Autowired
     private final JWTService jwtService;
+    @Autowired
     private final RegisterUserDTOToUserConverter registerUserDTOToUserConverter;
+    @Autowired
     private final EmailSenderService emailSenderService;
     private Users currentUser;
 
     public String registerUser(RegisterUserDto registerUserDto){
-        var convertedToUser = registerUserDTOToUserConverter.convertRegisterUserDTOToUser(registerUserDto);
-        convertedToUser.setRole(Role.USER);
-        convertedToUser.setPassword(passwordEncrypter.passwordEncoder()
-                .encode(registerUserDto.getPassword()));
-        this.currentUser = convertedToUser;
-        return "Verification code Sent";
+        try {
+            var convertedToUser = registerUserDTOToUserConverter.convertRegisterUserDTOToUser(registerUserDto);
+            convertedToUser.setRole(Role.USER);
+            convertedToUser.setPassword(passwordEncrypter.passwordEncoder()
+                    .encode(registerUserDto.getPassword()));
+            this.currentUser = convertedToUser;
+            emailSenderService.sendEmail(currentUser.getEmail());
+            return "Verification code Sent";
+        }catch (Exception e){
+            return e.getMessage();
+        }
+    }
+
+    public LoginUserResponseDTO confirmRegistration(String verificationCode){
+        if (emailSenderService.verifyOtp(verificationCode)){
+            userRepository.save(currentUser);
+            var token = jwtService.generateToken(currentUser.getUsername());
+            return new LoginUserResponseDTO(currentUser.getUsername(),token,currentUser.getRole());
+        }else{
+            throw new InvalidVerificationCodeException("The given verification code is incorrect");
+        }
     }
 
     public LoginUserResponseDTO loginUser(LoginRequestDTO loginRequestDTO){
