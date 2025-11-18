6555f98ce20d49ed5e3f12dcc2e6b402b4810d45
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-11-18 16:33:56 +0100

    Implemented custom AxiosConfig.tsx

---
+++ fitnessfrontend/src/services/AxiosConfig.tsx
@@ -0,0 +1,48 @@
+import {UserStore} from "../stores/UserStore.ts";
+import axios from "axios";
+import {toast} from "react-toastify";
+
+
+const base_url = "/api/fitness";
+const getAcessToken =():string|undefined =>{
+    return UserStore.getState().user?.accessToken;
+}
+
+const api = axios.create({
+    baseURL:base_url,
+    headers:{
+        'Content-Type' : 'application/json'
+    },
+    withCredentials: true
+})
+
+api.interceptors.request.use(
+    (config) =>{
+        const token = getAcessToken();
+        if (token){
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config;
+    },
+    (error) =>{
+        return Promise.reject(error)
+    }
+)
+
+api.interceptors.response.use(
+    (response) =>{
+        return response;
+    },
+    (error) =>{
+        if (error.response && error.response.status === 401){
+            toast.error("Session timed out. Logging out!");
+
+            UserStore.getState().stateLogout();
+            window.location.href = "/login";
+            return Promise.reject(error);
+        }
+        return Promise.reject(error);
+    }
+)
+
+export default api;
