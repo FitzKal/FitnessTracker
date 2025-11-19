38cdf691d41c1d7522bca9e34f7445406b9786c7
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-11-19 15:12:22 +0100

    Implemented UserRegisterRequestStore.ts for storing register details temporarily

---
+++ fitnessfrontend/src/stores/UserRegisterRequestStore.ts
@@ -0,0 +1,27 @@
+import type {User} from "../types/User.ts";
+import {create} from "zustand/react";
+import { createJSONStorage, persist } from "zustand/middleware";
+interface RegisterState {
+    user: User | null,
+    stateRegister: (user:User, lastOTP : string, otpTime : string) => void,
+    stateEmpty: () => void
+    lastOTP : string | null,
+    otpTime : string | null
+}
+
+export const UserResisterRequestStore = create<RegisterState>()(
+    persist(
+        (set) =>({
+            user:null,
+            stateRegister:(user:User, lastOTP : string, otpTime : string) =>
+                set({user,lastOTP,otpTime}),
+            stateEmpty:() =>set({user:null, lastOTP:null, otpTime:null}),
+            lastOTP:null,
+            otpTime:null
+        }),
+        {
+            name:"TemporaryRegisterStore",
+            storage: createJSONStorage(() => localStorage)
+        }
+    )
+)
