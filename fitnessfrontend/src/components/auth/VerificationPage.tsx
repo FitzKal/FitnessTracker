4147801795f3797e6cbe1413dec32e50fc832cf9
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-11-28 20:05:30 +0100

    Added the required mutation and the button for handling resend verification code requests

--- fitnessfrontend/src/components/auth/VerificationPage.tsx
+++ fitnessfrontend/src/components/auth/VerificationPage.tsx
@@ -1,11 +1,11 @@
 import {useForm} from "react-hook-form";
 import type {Verification} from "../../types/FormTypes.ts";
 import {useMutation} from "@tanstack/react-query";
-import {confirmRegister} from "../../services/AuthService.ts";
+import {confirmRegister, resendVerificationCode} from "../../services/AuthService.ts";
 import {useNavigate} from "react-router-dom";
 import {toast} from "react-toastify";
 import {UserResisterRequestStore} from "../../stores/UserRegisterRequestStore.ts";
-import type {UserRegisterType} from "../../types/User.ts";
+import type {ResendTokenType, UserRegisterType} from "../../types/User.ts";
 
 
 function VerificationPage(){
@@ -34,22 +34,49 @@ function VerificationPage(){
         }
     })
 
+    const verificationMutation = useMutation({
+        mutationFn: (email : ResendTokenType) => resendVerificationCode(email),
+        onSuccess: (result) =>{
+            UserResisterRequestStore.getState().stateResetVerificationCode(
+                result.lastOTP,
+                result.otpTime
+            )
+            toast.success("Verification code has been resent! Check your email address!");
+        },
+        onError:(error) =>{
+            if (error instanceof  Error){
+                toast.error(error.message);
+            }else{
+                toast.error("Something went wrong!")
+            }
+        }
+    })
+
     const onSubmit = async (verification:Verification) =>{
         console.log(currentRegister)
         mutation.mutate(verification);
     }
 
+    const onResend = async () =>{
+        const email : ResendTokenType = {
+            email : currentRegister.user?.email
+        }
+        verificationMutation.mutate(email);
+    }
+
     return (<div className={"flex flex-col bg-linear-to-b from-blue-500 to-black min-h-screen"}>
         <h1 className={"flex self-center mt-10 mb-5 text-4xl text-white"}>Verify registered account</h1>
-        <div className={"flex self-center border-2 rounded-md bg-white h-100 w-130"}>
-            <form className={"flex flex-col self-center"} onSubmit={handleSubmit(onSubmit)}>
+        <div className={"flex flex-col self-center border-2 rounded-md bg-white h-100 w-130"}>
+            <form className={"flex flex-col self-center mt-22"} onSubmit={handleSubmit(onSubmit)}>
                 <input {...register("verificationCode")} placeholder={"Verification code"}
-                className={"border-2 mb-10 ml-25 rounded-md text-center w-75 h-10"}/>
-                <p className={"flex flex-wrap text-center mb-10"}>The verification code is only available for 30 minutes! Resend the code if it is expired!</p>
+                className={"border-2 mb-10 ml-13 rounded-md text-center w-75 h-10"}/>
+                <p className={"flex flex-wrap text-center mb-5 w-100"}>The verification code is only available for 30 minutes! Resend the code if it is expired!</p>
                 <button type={"submit"} className={"self-center bg-linear-to-b from-blue-500 to-black border-2 " +
                     "text-white rounded-md w-25 h-10 transition duration-300 ease-in-out hover:from-green-600 hover:to-black"}
                 >{isSubmitting ? "Loading..." : "Confirm"}</button>
             </form>
+            <button className={"border-2 rounded-md w-50 h-10 self-center mt-5 bg-blue-500 text-white transition duration-200 ease-in-out hover:bg-blue-700 "}
+            onClick={onResend}>Resend verification code</button>
         </div>
     </div>);
 }
