75114f0e1eb68114f8d27e8531cc46bae224f67e
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-12-11 23:07:53 +0200

    Added UserProfileRepository

---
+++ FitnessBackend/src/main/java/com/undieb/hu/main/Repositories/UserProfileRepository.java
@@ -0,0 +1,9 @@
+package com.undieb.hu.main.Repositories;
+
+import com.undieb.hu.main.Models.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserProfileRepository extends JpaRepository<UserProfile,Long> {
+}
