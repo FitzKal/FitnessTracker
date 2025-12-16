4f70bd7fec372ca2296999a60363f3eef015443a
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2025-12-16 12:05:12 +0200

    Implemented the necessary types for update profile form validation

--- fitnessfrontend/src/services/UserProfileService.ts
+++ fitnessfrontend/src/services/UserProfileService.ts
@@ -1,5 +1,6 @@
-// ------------- GetProfile -------------
+
 import api from "./AxiosConfig.ts";
+import type {UserUpdateRequest} from "../types/User.ts";
 
 export const getUserProfile = async () => {
     try {
@@ -9,6 +10,34 @@ export const getUserProfile = async () => {
         console.log(res);
         return res;
     }catch (error){
+        const message = (error as Error).message;
+        console.log(message);
+        return message;
+    }
+}
+
+// ------------- UpdateProfile -------------
+export const updateUserProfile = async (data:UserUpdateRequest, file:File ) => {
+    const formData = new FormData();
+    formData.append(
+        "data",
+        new Blob([JSON.stringify(data)], { type: "application/json" })
+    );
+
+    if (file) {
+        formData.append("file", file);
+    }
+
+    try {
+        const res = api.put("/profile",formData,{
+            headers:{
+                "Content-Type": undefined
+            }
+        })
+            .then(res => res.data);
+        console.log(res);
+        return res;
+    }catch(error){
         const message = (error as Error).message;
         console.log(message);
         return message;
--- fitnessfrontend/src/types/User.ts
+++ fitnessfrontend/src/types/User.ts
@@ -1,3 +1,7 @@
+import {z} from 'zod';
+const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"]
+
+
 export interface User {
     id?:number,
     username:string,
@@ -24,4 +28,29 @@ export interface UserProfile{
     weight?:number,
     profilePictureName?:string,
     profilePictureSrc?:string
+}
+
+export const UpdateSchema = z.object({
+    firstName:z.string().min(3,"The first name should be at least 3 characters long"),
+    lastName:z.string().min(3,"The last name should be at least 3 characters long"),
+    height:z.number(),
+    weight:z.number(),
+    image: z
+        .any()
+        .refine((files) => {
+            if (!files) return true;
+            if (files instanceof FileList && files.length === 0) return true;
+
+            const file = files[0];
+            return ACCEPTED_IMAGE_TYPES.includes(file.type);
+        }, "Invalid file type. Only JPEG, PNG, and WebP are allowed.")
+});
+
+export type UserUpdate = z.infer<typeof UpdateSchema>;
+
+export type UserUpdateRequest = {
+    firstName?:string,
+    lastName?:string,
+    height?:number,
+    weight?:number,
 }
