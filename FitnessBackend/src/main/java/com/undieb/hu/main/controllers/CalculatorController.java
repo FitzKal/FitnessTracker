cac06e4813578327b0a14c180a04cb3af7863f12
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2026-01-02 10:17:27 +0100

    feat: corrected base url

--- FitnessBackend/src/main/java/com/undieb/hu/main/controllers/CalculatorController.java
+++ FitnessBackend/src/main/java/com/undieb/hu/main/controllers/CalculatorController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("api/fitness")
+@RequestMapping("/api/fitness")
 public class CalculatorController {
     private final CalculatorService calculatorService;
 
