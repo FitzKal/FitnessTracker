8c9e4fbea586c998495caec22acaf961d94e4ef8
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2026-01-07 12:48:07 +0100

    feat: modified the getAll method to accept more optional parameters

--- FitnessBackend/src/main/java/com/undieb/hu/main/controllers/ExerciseController.java
+++ FitnessBackend/src/main/java/com/undieb/hu/main/controllers/ExerciseController.java
@@ -3,12 +3,15 @@ package com.undieb.hu.main.controllers;
 import com.undieb.hu.main.exercises.ResponseFromAPI;
 import com.undieb.hu.main.exercises.search.SearchResponseFromApi;
 import com.undieb.hu.main.exercises.searchById.SearchByIdResponse;
+import com.undieb.hu.main.exercises.types.BodyPart;
+import com.undieb.hu.main.exercises.types.Equipment;
+import com.undieb.hu.main.exercises.types.ExerciseType;
+import com.undieb.hu.main.exercises.types.Muscles;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/fitness/exercise")
@@ -24,14 +27,26 @@ public class ExerciseController {
     @GetMapping
     public ResponseEntity<ResponseFromAPI> getAllExercises(
             @RequestParam(required = false) String after,
-            @RequestParam(required = false) String before
-    ){
+            @RequestParam(required = false) String before,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false)ExerciseType exerciseType,
+            @RequestParam(required = false)BodyPart bodyPart,
+            @RequestParam(required = false)Equipment equipment,
+            @RequestParam(required = false)Muscles targetMuscles,
+            @RequestParam(required = false)Muscles secondaryMuscles
+            ){
         var response = webClient.get()
                 .uri(uriBuilder -> {
                     var builder = uriBuilder
                             .scheme(scheme)
                             .host(host)
                             .path(basePath + "exercises");
+                    if (name != null) builder.queryParam("name",name);
+                    if (exerciseType != null) builder.queryParam("exerciseType",exerciseType);
+                    if (bodyPart != null) builder.queryParam("bodyPart",bodyPart);
+                    if (equipment != null) builder.queryParam("equipment",equipment);
+                    if (targetMuscles != null) builder.queryParam("targetMuscles", targetMuscles);
+                    if (secondaryMuscles != null) builder.queryParam("secondaryMuscles",secondaryMuscles);
                     if (after != null) builder.queryParam("after", after);
                     if (before != null) builder.queryParam("before", before);
                     return builder.build();
