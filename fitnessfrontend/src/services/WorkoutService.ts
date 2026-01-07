7c1b73076f930c39f6d42268a13050785e082b85
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2026-01-07 12:50:28 +0100

    feat: added types for filtering workout, and the request for the backend

--- fitnessfrontend/src/services/WorkoutService.ts
+++ fitnessfrontend/src/services/WorkoutService.ts
@@ -1,12 +1,20 @@
 import api from "./AxiosConfig.ts";
+import type {workoutFilterRequest} from "../types/WorkoutType.ts";
 
 // ------------- GetAllWorkouts ------------
-export const getAllWorkouts = async (before?:string, after?:string) => {
+export const getAllWorkouts = async (before?:string, after?:string, filter?:workoutFilterRequest) => {
     try {
         return api.get("/exercise",{
             params:{
                 before:before?before:null,
-                after:after?after:null
+                after:after?after:null,
+                name:filter?.name,
+                exerciseType:filter?.exerciseType,
+                bodyPart:filter?.bodyPart,
+                equipment:filter?.equipment,
+                targetMuscles:filter?.targetMuscles,
+                secondaryMuscles:filter?.secondaryMuscles
+
             }
         })
             .then(res => res.data);
@@ -21,6 +29,23 @@ export const getAllWorkouts = async (before?:string, after?:string) => {
 export const getWorkoutById = async (exerciseId:string|undefined) =>{
     try{
         return api.get(`/exercise/${exerciseId}`).then(res => res.data)
+            .then(res => res.data)
+    }catch (error){
+        const message = (error as Error).message;
+        console.log(message);
+        return message;
+    }
+}
+
+// ------------- GetWorkoutsBySearch ------------
+export const getWorkoutBySearch = async (exerciseName:string|undefined) => {
+    try {
+        return api.get("/exercise/search",{
+            params:{
+                search:exerciseName
+            }
+        }).then(res => res.data)
+            .then(res => res.data);
     }catch (error){
         const message = (error as Error).message;
         console.log(message);
--- fitnessfrontend/src/types/WorkoutType.ts
+++ fitnessfrontend/src/types/WorkoutType.ts
@@ -1,4 +1,4 @@
-import {BodyPart, Equipment, Muscles} from "./WorkoutEnums.ts";
+import {BodyPart, Equipment, type ExerciseType, Muscles} from "./WorkoutEnums.ts";
 
 export interface WorkoutListElement{
     exerciseId:string,
@@ -8,4 +8,40 @@ export interface WorkoutListElement{
     equipments:Equipment[],
     secondaryMuscles:Muscles[],
     targetMuscles:Muscles[]
+}
+
+export interface WorkoutById{
+    exerciseId:string,
+    name:string,
+    equipments:Equipment[],
+    bodyParts:BodyPart[],
+    exerciseType:ExerciseType,
+    targetMuscles:Muscles[],
+    secondaryMuscles:Muscles[],
+    exerciseTips:string[],
+    variations:string[],
+    relatedExerciseIds:string[],
+    instructions:string[],
+    videoUrl:string,
+    imageUrl:string,
+    imageUrls:{
+        "480p":string,
+        "720p":string,
+        "1080p":string
+    }
+}
+
+export interface workoutFilterRequest {
+    name?:string,
+    targetMuscles?:Muscles,
+    secondaryMuscles?:Muscles,
+    exerciseType?:ExerciseType,
+    bodyPart?:BodyPart,
+    equipment?:Equipment
+}
+
+export interface WorkoutSearch{
+    exerciseId:string,
+    name?:string,
+    imageUrl:string,
 }
