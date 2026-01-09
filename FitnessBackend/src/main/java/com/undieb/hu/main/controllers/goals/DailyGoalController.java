75b2054f46acc0f4f0a9ba8d9d96ba7d794ec3a6
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2026-01-09 16:28:01 +0100

    feat: get requests for dailyGoals

--- FitnessBackend/src/main/java/com/undieb/hu/main/controllers/goals/DailyGoalController.java
+++ FitnessBackend/src/main/java/com/undieb/hu/main/controllers/goals/DailyGoalController.java
@@ -1,15 +1,30 @@
 package com.undieb.hu.main.controllers.goals;
 
+import com.undieb.hu.main.controllers.DTOs.goals.DailyGoalDTO;
 import com.undieb.hu.main.services.goals.DailyGoalService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/fitness/goal")
 public class DailyGoalController {
     private final DailyGoalService dailyGoalService;
+
+    @GetMapping("/dailyGoal/{id}")
+    public ResponseEntity<DailyGoalDTO> getDailyGoalById(@PathVariable Long id){
+        return ResponseEntity.ok(dailyGoalService.getDailyGoalById(id));
+    }
+
+    @GetMapping("/dailyGoal")
+    public ResponseEntity<List<DailyGoalDTO>> getAllDailyGoals(HttpServletRequest request){
+        return ResponseEntity.ok(dailyGoalService.getAllDailyGoals(request));
+    }
+
 
     @DeleteMapping("/deleteDailyGoal/{dailyGoalId}")
     public ResponseEntity<String> deleteDailyGoal(@PathVariable Long dailyGoalId){
--- FitnessBackend/src/main/java/com/undieb/hu/main/services/goals/DailyGoalService.java
+++ FitnessBackend/src/main/java/com/undieb/hu/main/services/goals/DailyGoalService.java
@@ -1,10 +1,17 @@
 package com.undieb.hu.main.services.goals;
 
+import com.undieb.hu.main.controllers.DTOs.goals.DailyGoalDTO;
 import com.undieb.hu.main.converters.GoalConverter;
+import com.undieb.hu.main.models.enums.DailyGoal;
 import com.undieb.hu.main.repositories.DailyGoalRepository;
+import com.undieb.hu.main.services.JWTService;
 import com.undieb.hu.main.services.helpers.MonthlyGoalHelper;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -12,6 +19,20 @@ public class DailyGoalService {
     private final MonthlyGoalHelper monthlyGoalHelper;
     private final DailyGoalRepository dailyGoalRepository;
     private final GoalConverter goalConverter;
+    private final JWTService jwtService;
+
+    public DailyGoalDTO getDailyGoalById(Long id){
+        var dailyGoal = monthlyGoalHelper.fetchDailyGoalById(id);
+        return goalConverter.dailyGoalToDailyGoalDTO(dailyGoal);
+    }
+
+    public List<DailyGoalDTO> getAllDailyGoals(HttpServletRequest request){
+        var username = jwtService.getUserFromRequest(request).getUsername();
+        return dailyGoalRepository.findAll().stream()
+                .filter(goal->goal.getWeeklyGoal().getMonthlyGoal()
+                        .getUserProfile().getUser().getUsername().equals(username))
+                .map(goalConverter::dailyGoalToDailyGoalDTO).collect(Collectors.toList());
+    }
 
     public String deleteDailyGoal(Long dailyGoalId){
         var dailyGoal = monthlyGoalHelper.fetchDailyGoalById(dailyGoalId);
