7741e2f9a9fa479928d626a2ef2e47d8cffe6f8b
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2026-01-09 16:05:33 +0100

    feat: implemented getAll for the goals

--- FitnessBackend/src/main/java/com/undieb/hu/main/controllers/goals/WeeklyGoalController.java
+++ FitnessBackend/src/main/java/com/undieb/hu/main/controllers/goals/WeeklyGoalController.java
@@ -2,9 +2,12 @@ package com.undieb.hu.main.controllers.goals;
 
 import com.undieb.hu.main.controllers.DTOs.goals.WeeklyGoalDTO;
 import com.undieb.hu.main.services.goals.WeeklyGoalService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/fitness/goal")
@@ -15,6 +18,11 @@ public class WeeklyGoalController {
     @GetMapping("/weeklyGoal/{id}")
     public ResponseEntity<WeeklyGoalDTO> getWeeklyGoalById(@PathVariable Long id){
         return ResponseEntity.ok(weeklyGoalService.getWeeklyGoalById(id));
+    }
+
+    @GetMapping("/weeklyGoal")
+    public ResponseEntity<List<WeeklyGoalDTO>> getAllWeeklyGoals(HttpServletRequest request){
+        return ResponseEntity.ok(weeklyGoalService.getAllWeeklyGoals(request));
     }
 
     @DeleteMapping("/deleteWeeklyGoal/{weeklyGoalId}")
--- FitnessBackend/src/main/java/com/undieb/hu/main/services/goals/WeeklyGoalService.java
+++ FitnessBackend/src/main/java/com/undieb/hu/main/services/goals/WeeklyGoalService.java
@@ -5,8 +5,12 @@ import com.undieb.hu.main.converters.GoalConverter;
 import com.undieb.hu.main.repositories.WeeklyGoalRepository;
 import com.undieb.hu.main.services.JWTService;
 import com.undieb.hu.main.services.helpers.MonthlyGoalHelper;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -20,6 +24,13 @@ public class WeeklyGoalService {
     public WeeklyGoalDTO getWeeklyGoalById(Long id){
         var weeklyGoal = monthlyGoalHelper.fetchWeeklyGoalById(id);
         return goalConverter.weeklyGoalToWeeklyGoalDTO(weeklyGoal);
+    }
+
+    public List<WeeklyGoalDTO> getAllWeeklyGoals(HttpServletRequest request){
+        var userName = jwtService.getUserFromRequest(request).getUsername();
+        return weeklyGoalRepository.findAll().stream()
+                .filter(goal ->goal.getMonthlyGoal().getUserProfile().getUser().getUsername().equals(userName))
+                .map(goalConverter::weeklyGoalToWeeklyGoalDTO).collect(Collectors.toList());
     }
 
     public String deleteWeeklyGoal(Long weeklyGoalId){
