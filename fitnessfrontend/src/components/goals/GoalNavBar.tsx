c8b87609fca3b74a90d788e53792db944143e3c1
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2026-01-15 13:56:51 +0100

    feat: ensured consistent `lg` height styling across CalculatorNavBar and GoalNavBar components

--- fitnessfrontend/src/components/goals/GoalNavBar.tsx
+++ fitnessfrontend/src/components/goals/GoalNavBar.tsx
@@ -3,7 +3,7 @@ import {Link, Outlet} from "react-router-dom";
 export default function GoalNavBar(){
     return (
         <div>
-            <div className="lg:fixed sm:relative top-0 w-full bg-red-800 text-white h-[6vh] flex items-center justify-center shadow-md">
+            <div className="lg:fixed sm:relative top-0 w-full bg-red-800 text-white lg:h-[6vh] flex items-center justify-center shadow-md">
                 <div className="flex flex-wrap justify-center gap-6">
                     <Link
                         to="/Fitness/goals/monthlyGoals"
