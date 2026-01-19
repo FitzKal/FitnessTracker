bdcc1892710b8915d7f27db10556d6fc179c5654
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2026-01-19 23:41:29 +0100

    feat: added email-based user queries in UserRepository
    - Introduced `existsByEmail` and `findByEmail` methods for email-based user checks.
    - Added `existsByUsername` method for username existence validation.

--- FitnessBackend/src/main/java/com/undieb/hu/main/repositories/UserRepository.java
+++ FitnessBackend/src/main/java/com/undieb/hu/main/repositories/UserRepository.java
@@ -7,4 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<Users,Long> {
     Users findByUsername(String username);
+    Boolean existsByEmail(String email);
+    Users findByEmail(String email);
+    Boolean existsByUsername(String username);
 }
