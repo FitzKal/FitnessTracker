023d570c993bbae28f797f0f8a1a7c570878f183
Vesa Bence Janos <vesabence@mailbox.unideb.hu>
2026-02-03 11:45:42 +0100

    feat: added AdminService with methods for managing users
    - Implemented `getAllUsers` to fetch user data.
    - Added `searchUsers` to enable user searches by keyword.

---
+++ fitnessfrontend/src/services/AdminService.ts
@@ -0,0 +1,31 @@
+import api from "./AxiosConfig.ts";
+
+//------------- GetAllUsers -------------
+export const getAllUsers = async () => {
+    try {
+        return api.get("/admin")
+            .then(res => res.data);
+    }catch (error){
+        if (error instanceof Error){
+            throw new Error(error.message);
+        }
+        throw error;
+    }
+}
+
+//------------- SearchUsers -------------
+export const searchUsers = async (keyWord:string) => {
+    try {
+        return api.post("/admin",null,{
+            params: {
+                keyWord: keyWord
+            }
+        })
+            .then(res => res.data);
+    }catch (error){
+        if (error instanceof Error){
+            throw new Error(error.message);
+        }
+        throw error;
+    }
+}
